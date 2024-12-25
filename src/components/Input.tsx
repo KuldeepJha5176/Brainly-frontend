e51e3aa765ad454af9b0c066ff011136ed97e16e
KuldeepJha5176 <ranacjha@gmail.com>
2024-12-25 22:00:29 +0530

    update

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -6,5 +6,6 @@ interface InputProps {
 export function Input({placeholder, reference}: InputProps) {
   return <div>
       <input ref={reference} placeholder={placeholder} type={"text"} className="px-4 py-2 border rounded m-2" ></input>
+      
   </div>
 }
