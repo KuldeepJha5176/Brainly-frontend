7349ade5389eb33348fda4b03a29aff6df6529d4
KuldeepJha5176 <ranacjha@gmail.com>
2024-12-31 23:20:44 +0530

    useed ref

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,12 +1,21 @@
+import { useRef } from "react";
 import { Input } from "../components/input";
 import { Button } from "../components/ui/Button";
 
 export function Signup() {
+    const UsernameRef = useRef();
+    const PasswordRef = useRef ();
+
+    function signup() {
+        const username = UsernameRef.current.value;
+        const password = PasswordRef.current.value;
+
+    }
     return (
     <div className="h-screen w-screen bg-gray-200 flex justify-center items-center">
     <div className="bg-white rounded-xl border min-w-48 p-8">
-        <Input  placeholder="Username" />
-        <Input  placeholder="Password" />
+        <Input ref={UsernameRef} placeholder="Username" />
+        <Input ref={PasswordRef} placeholder="Password" />
         <div className="flex justify-center pt-4">
             <Button  loading={false} variant="primary" text="Signup" fullWidth={true} />
         </div>
